9c4f233214a2e6f0e81f4d2929afb0878dd6650b
Derek <imzhangdeyu@qq.com>
2016-12-28 17:01:37 +0800

    init

--- web/README.md
+++ web/README.md
@@ -1,2 +1,6 @@
 # music
-## music zjj
+support vue + webpack + ES6 + stylus + pug
+
+TODO
+- [x] vue-router
+- [ ] vuex
---
+++ web/src/App.vue
@@ -0,0 +1,26 @@
+<style lang="stylus">
+#app
+    display: flex;
+    .name
+        background: #cccccc;
+        line-height: 20px;
+        text-align: center;
+        min-width: 60px;
+        height: 20px
+</style>
+<template lang="pug">
+#app
+    .name {{name}}
+    input(type="text", v-model="name")
+    router-view
+</template>
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            name: 'hello Derek'
+        }
+    }
+}
+</script>
